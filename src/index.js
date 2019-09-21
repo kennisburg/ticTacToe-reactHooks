bc12a175679ab67cc6cb2dcb3cb0997ff3c6b32b
kennisburg <kennyhong1@live.com>
2019-09-21 16:20:25 -0700

    click square, switch between X and O

--- src/index.js
+++ src/index.js
@@ -1,12 +1,84 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+
+function Square({ value, onClick }) {
+
+  return (
+    <button
+      className="square"
+      onClick={onClick}
+    >
+      {value}
+    </button>
+  );
+}
+
+
+
+function Board() {
+
+  const [ squares, setSquares ] = useState(Array(9).fill(null))
+  const [ isXNext, setIsXNext ] = useState(true)
+
+  function renderSquare(i) {
+    return <Square
+    value={squares[i]}
+    onClick={() => {
+      const nextSquares = squares.slice()
+      nextSquares[i] = isXNext ? 'X' : 'O'
+      setIsXNext(!isXNext)
+      setSquares(nextSquares)
+    }}
+    />;
+  }
+
+  const status = 'Next player:' + (isXNext ? 'X' : 'O');
+
+  return (
+    <div>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
+}
+
+
+
+function Game() {
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+      <div className="game-info">
+        <div>{/* status */}</div>
+        <ol>{/* TODO */}</ol>
+      </div>
+    </div>
+  );
+}
+
+// ========================================
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
