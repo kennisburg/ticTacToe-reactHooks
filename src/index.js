23619bbe6f32cc1ef6c266d9ac73869b664b1291
kennisburg <kennyhong1@live.com>
2019-09-21 18:58:30 -0700

    declare winner

--- src/index.js
+++ src/index.js
@@ -24,11 +24,22 @@ function Board() {
   const [ squares, setSquares ] = useState(Array(9).fill(null))
   const [ isXNext, setIsXNext ] = useState(true)
 
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = 'Winner: ' + winner;
+  } else {
+    status = 'Next player: ' + (isXNext ? 'X' : 'O');
+  }
+
   function renderSquare(i) {
     return <Square
     value={squares[i]}
     onClick={() => {
       const nextSquares = squares.slice()
+      if (calculateWinner(squares) || squares[i]) {
+        return;
+      }
       nextSquares[i] = isXNext ? 'X' : 'O'
       setIsXNext(!isXNext)
       setSquares(nextSquares)
@@ -36,7 +47,6 @@ function Board() {
     />;
   }
 
-  const status = 'Next player:' + (isXNext ? 'X' : 'O');
 
   return (
     <div>
@@ -63,6 +73,10 @@ function Board() {
 
 
 function Game() {
+
+  const [ history, setHistory ] = useState([{squares: Array(9).fill(null)}])
+  const [ isXNext, setIsXNext ] = useState(true)
+
   return (
     <div className="game">
       <div className="game-board">
@@ -74,6 +88,27 @@ function Game() {
       </div>
     </div>
   );
+}
+
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2], 
+    [3, 4, 5], 
+    [6, 7, 8], 
+    [0, 3, 6], 
+    [1, 4, 7], 
+    [2, 5, 8],
+    [0, 4, 8], 
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a]
+    }
+  }
+  return null;
 }
 
 // ========================================
